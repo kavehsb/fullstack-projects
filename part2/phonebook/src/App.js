081d5602218ea7e6918464270994d9ba5d462033
Kaveh Buenaventura <kavehsbuen@gmail.com>
2022-02-12 13:06:11 -0800

    Complete exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [nameToSearch, setNameToSearch] = useState('')
@@ -17,6 +13,15 @@ const App = () => {
   const filteredNames = persons.filter(
     person => person.name.toLowerCase().includes(nameToSearch.toLowerCase())
   )
+
+  const fetchInitialData = () => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  } 
+  useEffect(fetchInitialData, [])
 
   const addPerson = (event) => {
     event.preventDefault()
