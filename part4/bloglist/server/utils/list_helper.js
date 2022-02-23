84db5f6da08f4a9af8fc0ec40931ab7ae14c167d
Kaveh Buenaventura <kavehsbuen@gmail.com>
2022-02-22 17:29:51 -0800

    Complete exercise 4.4
    Updated eslint .env config. Added function to list_helper.js that calculates
    the total number of likes of a list of blogs and returns that sum. Also added
    tests to determine if the function is working as intended with sample data.

--- part4/bloglist/server/utils/list_helper.js
+++ part4/bloglist/server/utils/list_helper.js
@@ -1,7 +1,15 @@
-const dummy = (blogs) => {
-	return 1;
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+	const sum = blogs.reduce((likes, blog) => {
+		return likes + blog.likes;
+	}, 0);
+
+	return sum;
 };
 
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 };
