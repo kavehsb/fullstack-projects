17def601db774458cc840a012afcc5fa5bace51e
Kaveh Buenaventura <kavehsbuen@gmail.com>
2022-02-22 18:16:46 -0800

    Complete exercise 4.5
    Removed the dummy function and test. Added a function to
    find the blog with the most likes in a blog list and return it.
    If 2 blogs have the same number of likes it will return the first of
    those blogs encountered in the list. Added tests for this functionality
    and refactored the variable declarations in the test file to be global as they
    are and will be used multiple times while testing.

--- part4/bloglist/server/tests/listHelper.test.js
+++ part4/bloglist/server/tests/listHelper.test.js
@@ -1,82 +1,109 @@
+// Imports
 const listHelper = require('../utils/list_helper');
 
-test('dummy returns one', () => {
-	const blogs = [];
+// Variable declarations for tests (these are use multiple times during testing)
+const listWithOneBlog = [
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Go To Statement Considered Harmful',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+		likes: 5,
+		__v: 0
+	}
+];
 
-	const result = listHelper.dummy(blogs);
-	expect(result).toBe(1);
-});
+const listWithMultipleBlogs = [
+	{
+		_id: '5a422a851b54a676234d17f7',
+		title: 'React patterns',
+		author: 'Michael Chan',
+		url: 'https://reactpatterns.com/',
+		likes: 7,
+		__v: 0
+	},
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Go To Statement Considered Harmful',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+		likes: 5,
+		__v: 0
+	},
+	{
+		_id: '5a422b3a1b54a676234d17f9',
+		title: 'Canonical string reduction',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+		likes: 12,
+		__v: 0
+	},
+	{
+		_id: '5a422b891b54a676234d17fa',
+		title: 'First class tests',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+		likes: 12,
+		__v: 0
+	},
+	{
+		_id: '5a422ba71b54a676234d17fb',
+		title: 'TDD harms architecture',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+		likes: 0,
+		__v: 0
+	},
+	{
+		_id: '5a422bc61b54a676234d17fc',
+		title: 'Type wars',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+		likes: 2,
+		__v: 0
+	}
+];
 
+// Test suite for calculating total number of likes of a list of blogs
 describe('total likes', () => {
-	const listWithOneBlog = [
-		{
-			_id: '5a422aa71b54a676234d17f8',
-			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
-			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-			likes: 5,
-			__v: 0
-		}
-	];
 
 	test('list with one blog equals the likes of that list', () => {
 		const result = listHelper.totalLikes(listWithOneBlog);
 		expect(result).toBe(5);
 	});
 
-	const listWithMultipleBlogs = [
-		{
-			_id: '5a422a851b54a676234d17f7',
-			title: 'React patterns',
-			author: 'Michael Chan',
-			url: 'https://reactpatterns.com/',
-			likes: 7,
-			__v: 0
-		},
-		{
-			_id: '5a422aa71b54a676234d17f8',
-			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
-			url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-			likes: 5,
-			__v: 0
-		},
-		{
-			_id: '5a422b3a1b54a676234d17f9',
-			title: 'Canonical string reduction',
-			author: 'Edsger W. Dijkstra',
-			url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-			likes: 12,
-			__v: 0
-		},
-		{
-			_id: '5a422b891b54a676234d17fa',
-			title: 'First class tests',
-			author: 'Robert C. Martin',
-			url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-			likes: 10,
-			__v: 0
-		},
-		{
-			_id: '5a422ba71b54a676234d17fb',
-			title: 'TDD harms architecture',
-			author: 'Robert C. Martin',
-			url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-			likes: 0,
-			__v: 0
-		},
-		{
-			_id: '5a422bc61b54a676234d17fc',
-			title: 'Type wars',
-			author: 'Robert C. Martin',
-			url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-			likes: 2,
-			__v: 0
-		}
-	];
-
 	test('list with multiple blogs has likes equal to the sum ', () => {
 		const result = listHelper.totalLikes(listWithMultipleBlogs);
-		expect(result).toBe(36);
+		expect(result).toBe(38);
+	});
+});
+
+// Test suite for returning the blog with the most amount of likes
+describe('favorite blog', () => {
+
+	// Test if the blog with the most likes is returned from a list of only
+	// one blog
+	test('favorite blog from list with one blog', () => {
+		const result = listHelper.favoriteBlog(listWithOneBlog);
+		expect(result).toEqual(
+			{
+				title: 'Go To Statement Considered Harmful',
+				author: 'Edsger W. Dijkstra',
+				likes: 5,
+			}
+		);
+	});
+
+	// Testing if the blog with the most amount og likes is returned from
+	// a list of multiple blogs
+	test('favorite blog from list of multiple blogs', () => {
+		const result = listHelper.favoriteBlog(listWithMultipleBlogs);
+		expect(result).toEqual(
+			{
+				title: 'Canonical string reduction',
+				author: 'Edsger W. Dijkstra',
+				likes: 12,
+			},
+		);
 	});
 });
--- part4/bloglist/server/utils/list_helper.js
+++ part4/bloglist/server/utils/list_helper.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-const dummy = (blogs) => 1;
-
+// Calculate total number of likes for a list of blogs
 const totalLikes = (blogs) => {
 	const sum = blogs.reduce((likes, blog) => {
 		return likes + blog.likes;
@@ -9,7 +7,23 @@ const totalLikes = (blogs) => {
 	return sum;
 };
 
+// Find the blog with the most amount of likes and return it
+const favoriteBlog = (blogs) => {
+	const favorite = blogs.reduce((favBlog, blog) => {
+		if (Object.keys(favBlog).length === 0) {
+			return filter(blog);
+		}
+		return filter(favBlog.likes < blog.likes ? blog : favBlog);
+	}, {});
+
+	return favorite;
+};
+
+// Helper function to filter out properties of a returned blog
+const filter = ({ title, author, likes }) => ({ title, author, likes });
+
+// Exports
 module.exports = {
-	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 };
